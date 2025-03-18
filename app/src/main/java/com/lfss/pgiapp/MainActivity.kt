271643736996975c054da5ee7f3ecde659b5afc2
Luiz Sa <28lfss@gmail.com>
2025-03-18 10:21:32 -0300

    Create Occurrence layout added

--- app/src/main/java/com/lfss/pgiapp/MainActivity.kt
+++ app/src/main/java/com/lfss/pgiapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.lfss.pgiapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -21,12 +19,6 @@ class MainActivity : AppCompatActivity() {
         val qrCodeFragment = QrCodeFragment()
 
         setCurrentFragment(createOccurrenceFragment)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, 0, systemBars.right, 0)
-            insets
-        }
 
         bottomNavigationView.setOnNavigationItemSelectedListener {
             when (it.itemId) {
