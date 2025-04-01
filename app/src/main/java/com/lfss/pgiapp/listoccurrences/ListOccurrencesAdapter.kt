c57a50e36ae4dafdd8cdbf1903945b4f4beda715
28lfss <28lfss@gmail.com>
2025-04-01 18:03:23 -0300

    Style: Changed ListOccurrences values names.

--- app/src/main/java/com/lfss/pgiapp/listoccurrences/ListOccurrencesAdapter.kt
+++ app/src/main/java/com/lfss/pgiapp/listoccurrences/ListOccurrencesAdapter.kt
@@ -7,12 +7,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.lfss.pgiapp.R
 
-class ListOccurrencesAdapter(private val occurrenceList: List<ListOccurrencesFragment.Occurrence>):
-    RecyclerView.Adapter<ListOccurrencesAdapter.OccurrenceViewHolder>() {
+class ListOccurrencesAdapter(
+    private val occurrenceList: List<ListOccurrencesFragment.Occurrence>
+) : RecyclerView.Adapter<ListOccurrencesAdapter.OccurrenceViewHolder>() {
 
     class OccurrenceViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val areaTextView: TextView = view.findViewById(R.id.test_area)
-        val descriptionTextView: TextView = view.findViewById(R.id.test_description)
+        val areaTextView: TextView = view.findViewById(R.id.adapter_area)
+        val timeTextView: TextView = view.findViewById(R.id.adapter_time)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): OccurrenceViewHolder {
@@ -24,7 +25,7 @@ class ListOccurrencesAdapter(private val occurrenceList: List<ListOccurrencesFra
     override fun onBindViewHolder(viewHolder: OccurrenceViewHolder, position: Int) {
         val occurrence = occurrenceList[position]
         viewHolder.areaTextView.text = occurrence.area
-        viewHolder.descriptionTextView.text = occurrence.description
+        viewHolder.timeTextView.text = occurrence.time
     }
 
     override fun getItemCount() = occurrenceList.size
--- app/src/main/java/com/lfss/pgiapp/listoccurrences/ListOccurrencesFragment.kt
+++ app/src/main/java/com/lfss/pgiapp/listoccurrences/ListOccurrencesFragment.kt
@@ -7,31 +7,49 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.lfss.pgiapp.databinding.FragmentListOccurrencesBinding
 
-class ListOccurrencesFragment: Fragment() {
+class ListOccurrencesFragment : Fragment() {
 
     private val viewModel: ListOccurrencesViewModel by viewModels()
 
     private var _binding: FragmentListOccurrencesBinding? = null
     private val binding get() = _binding!!
 
-    data class Occurrence(val area: String, val description: String)
+    data class Occurrence(val area: String, val time: String)
 
     private val occurrenceList = listOf(
-        Occurrence("AREA 1", "DESCRIPTION 1"),
-        Occurrence("AREA 2", "DESCRIPTION 2"),
-        Occurrence("AREA 3", "DESCRIPTION 3"),
-        Occurrence("AREA 1", "DESCRIPTION 1"),
-        Occurrence("AREA 2", "DESCRIPTION 2"),
-        Occurrence("AREA 3", "DESCRIPTION 3")
-    )
+        Occurrence("AREA 1", "11111111111111"),
+        Occurrence("AREA 2", "22222222222222"),
+        Occurrence("AREA 3", "33333333333333"),
+        Occurrence("AREA 4", "44444444444444"),
+        Occurrence("AREA 5", "55555555555555"),
+        Occurrence("AREA 6", "66666666666666"),
+        Occurrence("AREA 1", "11111111111111"),
+        Occurrence("AREA 2", "22222222222222"),
+        Occurrence("AREA 3", "33333333333333"),
+        Occurrence("AREA 4", "44444444444444"),
+        Occurrence("AREA 5", "55555555555555"),
+        Occurrence("AREA 6", "66666666666666"),
+        Occurrence("AREA 1", "11111111111111"),
+        Occurrence("AREA 2", "22222222222222"),
+        Occurrence("AREA 3", "33333333333333"),
+        Occurrence("AREA 4", "44444444444444"),
+        Occurrence("AREA 5", "55555555555555"),
+        Occurrence("AREA 6", "66666666666666"),
+        Occurrence("AREA 1", "11111111111111"),
+        Occurrence("AREA 2", "22222222222222"),
+        Occurrence("AREA 3", "33333333333333"),
+        Occurrence("AREA 4", "44444444444444"),
+        Occurrence("AREA 5", "55555555555555"),
+        Occurrence("AREA 6", "66666666666666"),
+
+        )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstaceState: Bundle?
-    ) : View? {
+    ): View? {
         _binding = FragmentListOccurrencesBinding.inflate(inflater, container, false)
         return binding.root
     }
