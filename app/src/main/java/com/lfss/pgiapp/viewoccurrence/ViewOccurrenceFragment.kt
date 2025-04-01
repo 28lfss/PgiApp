46dd190088b5857cc12199a2b509bb16430e0c2d
28lfss <28lfss@gmail.com>
2025-04-01 18:05:05 -0300

    Feature: ViewOccurrence fragment created

---
+++ app/src/main/java/com/lfss/pgiapp/viewoccurrence/ViewOccurrenceFragment.kt
@@ -0,0 +1,31 @@
+package com.lfss.pgiapp.viewoccurrence
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lfss.pgiapp.databinding.FragmentViewOccurrenceBinding
+
+class ViewOccurrenceFragment : Fragment() {
+
+    private var _binding: FragmentViewOccurrenceBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstaceState: Bundle?
+    ): View? {
+        _binding = FragmentViewOccurrenceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
