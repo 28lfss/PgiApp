92b24fc2ae458e35d6a7ead9144a3e15c8b99d48
28lfss <28lfss@gmail.com>
2025-04-01 19:36:01 -0300

    Feature: Created Occurrence Model

---
+++ app/src/main/java/com/lfss/pgiapp/model/OccurrenceModel.kt
@@ -0,0 +1,11 @@
+package com.lfss.pgiapp.model
+
+import android.net.Uri
+
+class OccurrenceModel (
+    val id: Int?,
+    val area: String,
+    val description: String,
+    val imageUri: Uri?,
+    val timestamp: Long?
+)
