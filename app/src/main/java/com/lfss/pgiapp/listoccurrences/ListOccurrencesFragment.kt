48ba19c526f64809346dc00a3eed19c09c44ef8b
28lfss <28lfss@gmail.com>
2025-04-05 15:28:30 -0300

    Removed unused imports

--- app/src/main/java/com/lfss/pgiapp/listoccurrences/ListOccurrencesFragment.kt
+++ app/src/main/java/com/lfss/pgiapp/listoccurrences/ListOccurrencesFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lfss.pgiapp.R
 import com.lfss.pgiapp.databinding.FragmentListOccurrencesBinding
-import com.lfss.pgiapp.model.OccurrenceModel
 import com.lfss.pgiapp.viewoccurrence.ViewOccurrenceFragment
 
 class ListOccurrencesFragment : Fragment() {
