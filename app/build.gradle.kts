bb81b254def0be51036fa167d3d1ffd7ac27542a
28lfss <28lfss@gmail.com>
2025-05-10 16:05:59 -0300

    Adding retrofit converter gson dependency;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.zxing.android.embedded)
     implementation(libs.retrofit)
+    implementation (libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
