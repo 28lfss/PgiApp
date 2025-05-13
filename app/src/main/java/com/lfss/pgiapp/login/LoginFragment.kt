d56f8f897d7958b2f71bf02317dfefcb4839124d
28lfss <28lfss@gmail.com>
2025-05-13 19:14:44 -0300

    Minor: removing log

--- app/src/main/java/com/lfss/pgiapp/login/LoginFragment.kt
+++ app/src/main/java/com/lfss/pgiapp/login/LoginFragment.kt
@@ -64,7 +64,6 @@ class LoginFragment : Fragment() {
     }
 
     fun userLogin(email: String, password: String) {
-        //TODO: remove this later
-        viewModel.userLogin("test1@test.com", "test123")
+        viewModel.userLogin(email, password)
     }
 }
