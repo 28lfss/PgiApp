06435576e3f8849f56934655e2c8782924c4c85f
28lfss <28lfss@gmail.com>
2025-06-14 14:29:49 -0300

    feature: Send new occurrence request as form to API

--- app/src/main/java/com/lfss/pgiapp/api/PgiApi.kt
+++ app/src/main/java/com/lfss/pgiapp/api/PgiApi.kt
@@ -1,14 +1,30 @@
 package com.lfss.pgiapp.api
 
 import com.lfss.pgiapp.dto.UserLoginDTO
+import com.lfss.pgiapp.model.OccurrenceModel
 import com.lfss.pgiapp.model.UserModel
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface PgiApi {
+    // USER ENDPOINTS
     @POST("user/login")
     suspend fun userLogin(
         @Body user: UserLoginDTO
     ): Response<UserModel>
+
+    // OCCURRENCE ENDPOINTS
+    @Multipart
+    @POST("occurrence")
+    suspend fun createOccurrence(
+        @Part("area") area: RequestBody,
+        @Part("description") description: RequestBody,
+        @Part("occurrenceRegistrantId") occurrenceRegistrantId: RequestBody,
+        @Part imageFile: MultipartBody.Part
+    ): Response<OccurrenceModel>
 }
